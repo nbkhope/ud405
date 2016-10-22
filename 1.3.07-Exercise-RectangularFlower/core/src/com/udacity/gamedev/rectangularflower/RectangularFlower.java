b85a22f43512fb46f6a7bc8e4ba817328afeacc3
nbkhope <ryumemaru@yahoo.com>
2016-10-22 06:41:45 -0700

    Add solution to rectangular flower exercise & update gradle for original solution directory

--- 1.3.07-Exercise-RectangularFlower/core/src/com/udacity/gamedev/rectangularflower/RectangularFlower.java
+++ 1.3.07-Exercise-RectangularFlower/core/src/com/udacity/gamedev/rectangularflower/RectangularFlower.java
@@ -40,13 +40,32 @@ public class RectangularFlower extends ApplicationAdapter {
         shapeRenderer.rectLine(100, 0, 100, 300, 20);
 
         // TODO: Draw two leaves on the stem
+//        shapeRenderer.setColor(Color.MAGENTA);
+//        shapeRenderer.rect(40, 50, 0, 0, 60, 60, 1, 1, 0);
+
+        shapeRenderer.setColor(Color.YELLOW);
+        shapeRenderer.rect(40, 50, 40, 0, 60, 60, 1, 1, 35);
+
+        shapeRenderer.rect(100, 100, 30, 0, 60, 60, 1, 1, -35); // or 315 deg
 
         // TODO: Set the active color to yellow
+        shapeRenderer.setColor(Color.GRAY);
+
+        final int PETALS = 8;
 
         // TODO: Use a loop to draw 20 of these petals in a circle
+//        for (int i = 0; i < PETALS; i++) {
+//            shapeRenderer.rect(100, 300, 0, 0, 30, 20, 1, 1, i * 90 / PETALS);
+//        }
+       // shapeRenderer.rect(100, 300, 0, 0, 30, 20, 1, 1, 0);
 
-        float petalAngle = 45.0f;
-        shapeRenderer.rect(100, 300, 0, 0, 40, 40, 1, 1, petalAngle);
+        shapeRenderer.setColor(Color.YELLOW);
+
+        for (int i = 0; i < PETALS; i++) {
+            float petalAngle = 360.0f / PETALS; //45.0f;
+            shapeRenderer.rect(100, 300, 0, 0, 40, 40, 1, 1, petalAngle * i);
+        }
+
 
         shapeRenderer.end();
     }
